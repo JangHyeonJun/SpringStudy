f05af8ef1dbb062f81187bdef22acec8808a0d2e
JangHyeonJun <totok682@naver.com>
2022-09-04 22:02:43 +0900

    코드상에서 스프링 빈 직접 등록

--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     // 실무에서는 동시성 문제를 해결하기위해 Concurrent, Atomic 자료구조를 사용.

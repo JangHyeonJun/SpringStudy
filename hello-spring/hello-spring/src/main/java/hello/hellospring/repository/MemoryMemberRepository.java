68e4a8c67ef1443baf3a752e9cbc980a778c5d43
JangHyeonJun <totok682@naver.com>
2022-09-04 21:47:07 +0900

    컴포넌트 스캔 & 자동 의존관계 설정

--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     // 실무에서는 동시성 문제를 해결하기위해 Concurrent, Atomic 자료구조를 사용.

1547b4437747bde22ce1a03c8ffe3bc392c400f7
JangHyeonJun <totok682@naver.com>
2022-12-26 16:50:27 +0900

    스프링 컴포넌트 스캔(Component) 자동 의존성주입(AutoWired)

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -12,6 +12,8 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Configuration은 CGLIB이라는 내부 라이브러리를 통해서 AppConfig를 상속받은 객체를 컨테이너에 빈으로 등록한다.
+// CGLIB으로 생성된 AppConfig는 빈들이 한번만 생성되도록 막아주어 싱글톤 패턴을 지킬 수 있게 해준다.
 @Configuration
 public class AppConfig {
 
